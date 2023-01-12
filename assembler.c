7045016d17c7d65d9b92217ff0f657c06874d41e
Camille Custer <112519639+crcuster@users.noreply.github.com>
2023-01-11 21:57:40 -0700

    Add files via upload

---
+++ assembler.c
@@ -0,0 +1,309 @@
+//
+// Created by carson on 11/15/21.
+//
+
+#include "assembler.h"
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+char *ASM_ERROR_UNKNOWN_INSTRUCTION = "Unknown instruction";
+char *ASM_ERROR_ARG_REQUIRED = "Argument Required";
+char *ASM_ERROR_BAD_LABEL = "Bad Label";
+char *ASM_ERROR_OUT_OF_RANGE = "Number is out of range";
+
+//=========================================================
+//  All the instructions available on the LMSM architecture
+//=========================================================
+const char *INSTRUCTIONS[25] =
+        {"ADD", "SUB", "LDA", "STA", "BRA", "BRZ", "BRP", "INP", "OUT", "HLT", "COB", "DAT",
+         "LDI",
+         "CALL", "RET",
+         "SPUSH", "SPUSHI", "SPOP", "SDUP", "SADD", "SSUB", "SMAX", "SMIN", "SMUL", "SDIV"
+        };
+const int INSTRUCTION_COUNT = 25;
+
+//===================================================================
+//  All the instructions that require an arg on the LMSM architecture
+//===================================================================
+const char *ARG_INSTRUCTIONS[11] =
+        {"ADD", "SUB", "LDA", "STA", "BRA", "BRZ", "BRP", "DAT",
+         "LDI",
+         "CALL",
+         "SPUSHI"
+        };
+const int ARG_INSTRUCTION_COUNT = 11;
+
+//======================================================
+// Constructors/Destructors
+//======================================================
+
+instruction * asm_make_instruction(char* type, char *label, char *label_reference, int value, instruction * predecessor) {
+    instruction *new_instruction = calloc(1, sizeof(instruction));
+    new_instruction->instruction = type;
+    new_instruction->label = label;
+    new_instruction->label_reference = label_reference;
+    new_instruction->value = value;
+    new_instruction->next = NULL;
+
+    if (strcmp("SPUSHI", type) == 0){
+        new_instruction->slots = 2;
+    }else if (strcmp("CALL", type) == 0){
+        new_instruction->slots = 3;
+    } else{
+        new_instruction->slots = 1;
+    }
+    if (predecessor != NULL) {
+        predecessor->next = new_instruction;
+        new_instruction->offset = predecessor->offset + predecessor->slots;
+    } else {
+        new_instruction->offset = 0;
+    }
+
+    return new_instruction;
+}
+
+void asm_delete_instruction(instruction *instruction) {
+    if (instruction == NULL) {
+        return;
+    }
+    asm_delete_instruction(instruction->next);
+    free(instruction);
+}
+
+compilation_result * asm_make_compilation_result() {
+    compilation_result *result = calloc(1, sizeof(compilation_result));
+    return result;
+}
+
+void asm_delete_compilation_result(compilation_result *result) {
+    asm_delete_instruction(result->root);
+    free(result);
+}
+
+//======================================================
+// Helpers
+//======================================================
+int asm_is_instruction(char * token) {
+    for (int i = 0; i < INSTRUCTION_COUNT; ++i) {
+        if (strcmp(token, INSTRUCTIONS[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int asm_instruction_requires_arg(char * token) {
+    for (int i = 0; i < ARG_INSTRUCTION_COUNT; ++i) {
+        if (strcmp(token, ARG_INSTRUCTIONS[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int asm_is_num(char * token){
+    if (*token == '-') { // allow a leading negative
+        token++;
+    }
+    while (*token != '\0') {
+        if (*token < '0' || '9' < *token) {
+            return 0;
+        }
+        token++;
+    }
+    return 1;
+}
+
+int asm_find_label(instruction *root, char *label) {
+    //TODO - scan the linked list for the given label, return -1 if not found
+    while(root != NULL) {
+        if (root->label != NULL){
+            if (strcmp(root->label, label) == 0) {
+                return root->value;
+            }else if (root->next != NULL){
+                root = root->next;
+            }else{
+                return -1;
+            }
+        }
+        root = root->next;
+    }
+    return -1;
+}
+
+
+//======================================================
+// Assembly Parsing/Scanning
+//======================================================
+
+void asm_parse_src(compilation_result * result, char * original_src){
+
+    // copy over so strtok can mutate
+    char * src = calloc(strlen(original_src) + 1, sizeof(char));
+    strcat(src, original_src);
+
+    instruction * last_instruction = NULL;
+    instruction * current_instruction = NULL;
+
+    char *token = strtok(src, " \n");
+    while(token != NULL) {
+
+        char *label = NULL;
+        if (!asm_is_instruction(token)) {
+            label = token;
+            token = strtok(NULL, " \n");
+
+        }
+        //handle the instruction
+        char *instruction = NULL;
+        if (!(asm_is_instruction(token))) {
+            result->error = ASM_ERROR_UNKNOWN_INSTRUCTION;
+            return;
+        }else{
+            instruction = token;
+        }
+        int value = 0;
+        char *label_reference = NULL;
+        if (asm_instruction_requires_arg(instruction)) {
+            token = strtok(NULL, " \n");
+            if (token == NULL) {
+                result->error = ASM_ERROR_ARG_REQUIRED;
+                return;
+            }
+
+            if (!asm_is_num(token)) {
+                label_reference = token;
+            }
+            if (asm_is_num(token)) {
+                value = atoi(token);
+                if (value > 999 || value < -999) {
+                    result->error = ASM_ERROR_OUT_OF_RANGE;
+                    return;
+                }
+            }
+        }
+
+        current_instruction = asm_make_instruction(instruction,label, label_reference, value, last_instruction);
+        if (result->root == NULL) {
+            result->root = current_instruction;
+        } else {
+            result->root->next = current_instruction;
+        }
+        last_instruction = current_instruction;
+        token = strtok(NULL, " \n");
+    }
+
+
+    //result->root->instruction = original_src;
+    //       generate the following errors as appropriate:
+    //
+    //       ASM_ERROR_UNKNOWN_INSTRUCTION - when an unknown instruction is encountered
+    //       ASM_ERROR_ARG_REQUIRED        - when an instruction does not have a proper argument passed to it
+    //       ASM_ERROR_OUT_OF_RANGE        - when a number argument is out of range (-999 to 999)
+    //
+
+
+}
+
+//======================================================
+// Machine Code Generation
+//======================================================
+
+void asm_gen_code_for_instruction(compilation_result  * result, instruction *instruction) {
+
+    //TODO - generate the machine code for the given instruction
+    //
+    // note that some instructions will take up multiple slots
+    //
+    // note that if the instruction has a label reference rather than a raw number reference
+    // you will need to look it up with `asm_find_label` and, if the label does not exist,
+    // report the error as ASM_ERROR_BAD_LABEL
+
+
+    int value_for_instruction = instruction->value;
+    if(instruction->label_reference != NULL){
+        value_for_instruction = asm_find_label(result->root, instruction->label_reference);
+        if(value_for_instruction == -1) {
+            result->error = ASM_ERROR_BAD_LABEL;
+            return;
+        }
+    }
+
+    if (strcmp("ADD", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 100 + value_for_instruction;
+    } else if (strcmp("SUB", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 200 + value_for_instruction;
+    } else if (strcmp("STA", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 300 + value_for_instruction;
+    } else if (strcmp("LDI", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 400 + value_for_instruction;
+    } else if (strcmp("LDA", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 500 + value_for_instruction;
+    } else if (strcmp("BRA", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 600 + value_for_instruction;
+    } else if (strcmp("BRZ", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 700 + value_for_instruction;
+    } else if (strcmp("BRP", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 800 + value_for_instruction;
+    } else if (strcmp("INP", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 901;
+    } else if (strcmp("OUT", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 902;
+    } else if (strcmp("HLT", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 000;
+    }else if (strcmp("COB", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 000;
+    }else if (strcmp("DAT", instruction->instruction) == 0) {
+        result->code[instruction->offset] = value_for_instruction;
+    }else if (strcmp("SPUSH", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 920;
+    }else if (strcmp("SPOP", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 921;
+    }else if (strcmp("SDUP", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 922;
+    }else if (strcmp("SADD", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 923;
+    }else if (strcmp("SSUB", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 924;
+    }else if (strcmp("SMAX", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 925;
+    }else if (strcmp("SMIN", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 926;
+    }else if (strcmp("SMUL", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 927;
+    }else if (strcmp("SDIV", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 928;
+    }else if (strcmp("SPUSHI", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 920;
+        result->code[instruction->offset + 1] = 400 + value_for_instruction;
+    }else if (strcmp("CALL", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 920;
+        result->code[instruction->offset + 1] = 400 + value_for_instruction;
+        result->code[instruction->offset + 2] = 910;
+    }else if (strcmp("RET", instruction->instruction) == 0) {
+        result->code[instruction->offset] = 911;
+    }else {
+        result->code[instruction->offset] = 0;
+    }
+
+}
+
+void asm_gen_code(compilation_result * result) {
+    instruction * current = result->root;
+    while (current != NULL) {
+        asm_gen_code_for_instruction(result, current);
+        current = current->next;
+    }
+}
+
+//======================================================
+// Main API
+//======================================================
+
+compilation_result * asm_assemble(char *src) {
+    compilation_result * result = asm_make_compilation_result();
+    asm_parse_src(result, src);
+    asm_gen_code(result);
+    return result;
+}
---
+++ lmsm.c
@@ -0,0 +1,358 @@
+#include "lmsm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+//======================================================
+//  Instruction Implementation
+//======================================================
+
+
+
+int check_stack(lmsm *our_little_machine) {
+    lmsm_stack *current = our_little_machine->accumulator;
+    if (current == NULL) {
+        our_little_machine->status = STATUS_HALTED;
+        our_little_machine->error_code = ERROR_EMPTY_STACK;
+        return 0;
+    }
+    lmsm_stack *next = current->next;
+    if (next == NULL) {
+        our_little_machine->status = STATUS_HALTED;
+        our_little_machine->error_code = ERROR_EMPTY_STACK;
+        return 0;
+    }
+    return 1;
+}
+void lmsm_i_push(lmsm *our_little_machine) {
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = 0;
+    new->next = current;
+    our_little_machine->accumulator = new;
+}
+
+void lmsm_i_pop(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)){
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *new = current->next;
+    our_little_machine->accumulator = new; //update the accumulator
+
+}
+
+void lmsm_i_dup(lmsm *our_little_machine) {
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = current->value;
+    new->next = current;
+    our_little_machine->accumulator = new;
+}
+
+void lmsm_i_call(lmsm *our_little_machine) {
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = our_little_machine->program_counter;
+    new->next = our_little_machine->call_stack;
+    our_little_machine->call_stack = new;
+    our_little_machine -> program_counter = current->value;
+    lmsm_i_pop(our_little_machine);
+
+}
+
+void lmsm_i_return(lmsm *our_little_machine) {
+    lmsm_stack *current = our_little_machine->call_stack;
+    our_little_machine->program_counter = current->value;
+    lmsm_stack *new = current->next;
+    our_little_machine->call_stack = new;
+    //consume value on top of call stack
+    //set thte program counter to that value
+}
+
+void lmsm_i_sadd(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = current->value + next->value;
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+}
+
+void lmsm_i_ssub(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = next->value - current->value;          //subtract top value from second value
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+
+}
+
+void lmsm_i_smax(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    if (current->value > next->value) {
+        new->value = current->value;
+    }else {
+        new->value = next->value;
+    }
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+}
+
+void lmsm_i_smin(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    if (current->value < next->value) {
+        new->value = current->value;
+    }else {
+        new->value = next->value;
+    }
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+}
+
+void lmsm_i_smul(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = next->value * current->value;          //subtract top value from second value
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+
+}
+
+void lmsm_i_sdiv(lmsm *our_little_machine) {
+    if (!check_stack(our_little_machine)) {
+        return;
+    }
+    lmsm_stack *current = our_little_machine->accumulator;
+    lmsm_stack *next = current->next;
+    lmsm_stack *new = malloc(sizeof(lmsm_stack));
+    new->value = next->value / current->value;          //subtract top value from second value
+    new->next = next->next;
+    our_little_machine->accumulator = new;
+
+    free(current);
+    free(next);
+}
+
+void lmsm_i_out(lmsm *our_little_machine) {
+     char temp[4];
+     char *out = our_little_machine->output_buffer;
+     int i = our_little_machine->accumulator->value;
+
+     sprintf(temp, "%d", i);
+     strcat(temp, " ");
+     strcat(out, temp);
+}
+
+void lmsm_i_inp(lmsm *our_little_machine) {
+}
+
+void lmsm_i_load(lmsm *our_little_machine, int location) {
+    our_little_machine->accumulator->value = our_little_machine->memory[location];
+}
+
+void lmsm_i_add(lmsm *our_little_machine, int location) {
+    our_little_machine->accumulator->value += our_little_machine->memory[location];
+}
+
+void lmsm_i_sub(lmsm *our_little_machine, int location) {
+    our_little_machine->accumulator->value -= our_little_machine->memory[location];
+}
+
+void lmsm_i_load_immediate(lmsm *our_little_machine, int value) {
+    our_little_machine->accumulator->value = value;
+}
+
+void lmsm_i_store(lmsm *our_little_machine, int location) {
+    our_little_machine->memory[location] = our_little_machine->accumulator->value;
+}
+
+void lmsm_i_halt(lmsm *our_little_machine) {
+    our_little_machine->status = STATUS_HALTED;
+}
+
+void lmsm_i_branch_unconditional(lmsm *our_little_machine, int location) {
+    our_little_machine->program_counter=location;
+}
+
+void lmsm_i_branch_if_zero(lmsm *our_little_machine, int location) {
+    if (our_little_machine->accumulator->value == 0) {
+        our_little_machine->program_counter = location;
+    }
+}
+
+void lmsm_i_branch_if_positive(lmsm *our_little_machine, int location) {
+    if (our_little_machine->accumulator->value >= 0) {             //if accumulator greater than or equal to 0
+    our_little_machine->program_counter = location;
+    }
+}
+
+void lmsm_cap_accumulator_value(lmsm *our_little_machine){
+    if(our_little_machine->accumulator->value > 999){               //cap value if its out of our range
+        our_little_machine->accumulator->value = 999;
+    }
+    else if (our_little_machine->accumulator->value < -999){
+        our_little_machine->accumulator->value = -999;
+    }
+}
+
+void lmsm_step(lmsm *our_little_machine) {
+    if (our_little_machine->status != STATUS_HALTED) {
+        int next_instruction = our_little_machine->memory[our_little_machine->program_counter];
+        our_little_machine->program_counter++;
+        our_little_machine->current_instruction = next_instruction;
+        lmsm_exec_instruction(our_little_machine, our_little_machine->current_instruction);
+    }
+}
+
+//======================================================
+//  LMSM Implementation
+//======================================================
+
+void lmsm_exec_instruction(lmsm *our_little_machine, int instruction) {
+
+    // TODO - dispatch the rest of the instruction set and implement
+    //        the instructions above
+
+    if (instruction == 0) {
+        lmsm_i_halt(our_little_machine);
+    } else if (100 <= instruction && instruction <= 199) {
+        lmsm_i_add(our_little_machine, instruction - 100);
+    } else if (200 <= instruction && instruction <= 299) {
+        lmsm_i_sub(our_little_machine, instruction - 200);
+    } else if (300 <= instruction && instruction <= 399) {
+        lmsm_i_store(our_little_machine, instruction - 300);
+    } else if (400 <= instruction && instruction <= 499) {
+        lmsm_i_load_immediate(our_little_machine, instruction - 400);
+    } else if (520 == instruction) {
+        lmsm_i_load(our_little_machine, instruction - 500);
+    } else if (600 <= instruction && instruction <= 699) {
+        lmsm_i_branch_unconditional(our_little_machine, instruction - 600);
+    } else if (800 <= instruction && instruction <= 899) {
+        lmsm_i_branch_if_positive(our_little_machine, instruction - 800);
+    } else if (700 <= instruction && instruction <= 799) {
+        lmsm_i_branch_if_zero(our_little_machine, instruction - 700);
+    } else if (910 == instruction) {
+        lmsm_i_call(our_little_machine);
+    } else if (911 == instruction) {
+        lmsm_i_return(our_little_machine);
+    } else if (920 == instruction) {
+        lmsm_i_push(our_little_machine);
+    } else if (921 == instruction) {
+        lmsm_i_pop(our_little_machine);
+    } else if (922 == instruction) {
+        lmsm_i_dup(our_little_machine);
+    } else if (902 == instruction) {
+        lmsm_i_out(our_little_machine);
+    } else if (923 == instruction) {
+        lmsm_i_sadd(our_little_machine);
+    } else if (924 == instruction) {
+        lmsm_i_ssub(our_little_machine);
+    }else if (925 == instruction) {
+        lmsm_i_smax(our_little_machine);
+    }else if (926 == instruction) {
+        lmsm_i_smin(our_little_machine);
+    }else if (927 == instruction) {
+        lmsm_i_smul(our_little_machine);
+    }else if (928 == instruction) {
+        lmsm_i_sdiv(our_little_machine);
+    } else{
+        our_little_machine->error_code = ERROR_UNKNOWN_INSTRUCTION;
+            our_little_machine->status = STATUS_HALTED;
+        }
+
+    lmsm_cap_accumulator_value(our_little_machine);
+}
+
+void lmsm_load(lmsm *our_little_machine, int *program, int length) {
+    for (int i = 0; i < length; ++i) {
+        our_little_machine->memory[i] = program[i];
+    }
+}
+
+void lmsm_delete_stack(lmsm_stack *current) {
+    if (current->next) {
+        lmsm_delete_stack(current->next);
+    }
+    free(current);
+}
+
+void lmsm_init(lmsm *the_machine) {
+    the_machine->accumulator->value = 0;
+    the_machine->accumulator->next = NULL;
+    the_machine->status = STATUS_READY;
+    the_machine->error_code = ERROR_NONE;
+    the_machine->program_counter = 0;
+    the_machine->current_instruction = 0;
+    the_machine->call_stack->value = 0;
+    the_machine->call_stack->next = NULL;
+    memset(the_machine->output_buffer, 0, sizeof(char) * 1000);
+    memset(the_machine->memory, 0, sizeof(int) * 100);
+}
+
+void lmsm_reset(lmsm *our_little_machine) {
+    lmsm_delete_stack(our_little_machine->accumulator);
+    our_little_machine->accumulator = malloc(sizeof(lmsm_stack));
+    lmsm_delete_stack(our_little_machine->call_stack);
+    our_little_machine->call_stack = malloc(sizeof(lmsm_stack));
+    lmsm_init(our_little_machine);
+}
+
+void lmsm_run(lmsm *our_little_machine) {
+    our_little_machine->status = STATUS_RUNNING;
+    while (our_little_machine->status != STATUS_HALTED) {
+        lmsm_step(our_little_machine);
+    }
+}
+
+lmsm *lmsm_create() {
+    lmsm *the_machine = malloc(sizeof(lmsm));
+    the_machine->accumulator = malloc(sizeof(lmsm_stack));
+    the_machine->call_stack = malloc(sizeof(lmsm_stack));
+    lmsm_init(the_machine);
+    return the_machine;
+}
+
+void lmsm_delete(lmsm *the_machine) {
+    lmsm_delete_stack(the_machine->accumulator);
+    lmsm_delete_stack(the_machine->call_stack);
+    free(the_machine);
+}
